fbfb5bbba8c24f5db9238955a8c2a520fd391473
Robin Theilade <robin.theilade@gmail.com>
2015-09-06 13:33:25 +0200

    Release 2015.09.06.1331

--- Code/UnityContrib.UnityEngine/Properties/AssemblyInfo.cs
+++ Code/UnityContrib.UnityEngine/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "2015.08.25.*" )]
-[assembly: AssemblyFileVersion( "2015.08.25.0801" )]
+[assembly: AssemblyVersion( "2015.09.06.*" )]
+[assembly: AssemblyFileVersion( "2015.09.06.1331" )]
